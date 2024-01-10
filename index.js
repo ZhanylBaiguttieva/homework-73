45af654a83ee182892e3661aaf50e0fcce5e7860
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2024-01-10 23:10:43 +0600

    Added app.get and app.listen

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+
+
+app.get('/:name',(req,res) => {
+    res.send(req.params.name);
+});
+
+app.listen(port,() => {
+    console.log('Server online on ' + port);
+});
