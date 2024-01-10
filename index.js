a0c3b2c985210766878569e49e1606812b345763
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2024-01-10 23:48:06 +0600

    Added key as password and added app.get to encode/decode

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-
+const Vigenere = require('caesar-salad').Vigenere;
+const key = 'milka';
 
 app.get('/:name',(req,res) => {
     res.send(req.params.name);
 });
 
+app.get('/encode/:name',(req,res) => {
+    res.send(Vigenere.Cipher(key).crypt(req.params.name));
+});
+
+app.get('/decode/:name',(req,res) => {
+    res.send(Vigenere.Decipher(key).crypt(req.params.name));
+});
+
 app.listen(port,() => {
     console.log('Server online on ' + port);
 });
+
